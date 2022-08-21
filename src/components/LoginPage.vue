3fd067baf858c1e3e41871fca829afc53c4529b9
BigBloop <ansomofficial@gmail.com>
2022-08-21 15:59:20 +0800

    added new files

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-center items-center flex-col">
+    <div class="flex flex-col">
+      <label for="" class="">User</label>
+      <input type="text" class="border p-1" placeholder="Username">
+      <label for="">Password</label>
+      <input type="password" class="border p-1" placeholder="Password">
+    </div>
+  </div>
+</template>
